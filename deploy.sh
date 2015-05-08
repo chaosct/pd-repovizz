c1b71cee1729638d7e92311c2dc092b3e98bb37c
Carles F. Julià <carles.fernandez@upf.edu>
2015-05-08 12:37:19 +0200

    better scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-virtualenv --system-site-packages env
-. env/bin/activate
-pip install -r requirements.txt
 
-ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future easy_install bbfreeze
+command -v pip >/dev/null 2>&1 || { echo "pip not found, install with 'sudo easy_install pip'" >&2; exit 1; }
+command -v virtualenv >/dev/null 2>&1 || { echo "virtualenv not found. Install with pip install virtualenv" >&2; exit 1; }
+
+virtualenv --system-site-packages env
+env/bin/pip install -r requirements.txt
+
+ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future env/bin/easy_install bbfreeze
 
